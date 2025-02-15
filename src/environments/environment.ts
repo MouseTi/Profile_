144e7ac95f0f214e0751fe18c522b9982f85f81d
alt <hdang14203@gmail.com>
2025-02-16 05:10:10 +0700

    Update

---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: true,
+  apiBaseUrl: 'https://khaidevapi.onrender.com/discord',
+  "discordId": "1036134354566525029",
+  "default": {
+    "banner": "https://cdn.discordapp.com/assets/profile_effects/effects/2024-07-30/vct_supernova/intro.png",
+    "avatar_decoration": "a_90e0dce3cc48c4a9607b6d41209c737e.png",
+    "theme_colors": ['#0501FA', '#FA0101'],
+    "profile_effect": {
+      "intro": "",
+      "loop": "https://cdn.discordapp.com/assets/profile_effects/effects/2024-07-30/vct_supernova/intro.png",
+    }
+  }
+};
